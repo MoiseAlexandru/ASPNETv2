22be2c6fa9d9de24579dfe1ae2f21f3da5f198ed
moise_alexandru <moise.alexandru.ichb@gmail.com>
2023-02-04 10:05:57 +0200

    Adaugat repository pentru Profile

---
+++ ASPNETv2/Repository/ProfileRepository/IProfileRepository.cs
@@ -0,0 +1,11 @@
+﻿using ASPNETv2.Models;
+using ASPNETv2.Repository.GenericRepository;
+
+namespace ASPNETv2.Repository.ProfileRepository
+{
+    public interface IProfileRepository : IGenericRepository <Profile>
+    {
+        List<Profile> GetAllWithInclude();
+        List<Profile> GetAllWithJoin();
+    }
+}
---
+++ ASPNETv2/Repository/ProfileRepository/ProfileRepository.cs
@@ -0,0 +1,35 @@
+﻿using ASPNETv2.Data;
+using ASPNETv2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETv2.Repository.ProfileRepository
+{
+    public class ProfileRepository : GenericRepository.GenericRepository<Profile>, IProfileRepository
+    {
+        public ProfileRepository(DatabaseContext context) : base(context) { }
+        public List<Profile> GetAllWithInclude()
+        {
+            return _table.Include(profile => profile.User).Include(profile => profile.Groups).Include(profile => profile.Notes).ToList();
+        }
+
+        public async Task <List<Profile>> GetAllWithIncludeAsync()
+        {
+            return await _table.Include(profile => profile.User).Include(profile => profile.Groups).Include(profile => profile.Notes).ToListAsync();
+        }
+
+        // intre useri si profiluri de ex
+        public List<Profile> GetAllWithJoin()
+        {
+            var result = _table.Join(_context.Profiles, profile => profile.ProfileId, user => user.ProfileId,
+                (profile, user) => new { profile, user }).Select(obj => obj.profile);
+            return result.ToList();
+        }
+
+        public async Task <List<Profile>> GetAllWithJoinAsync()
+        {
+            var result = _table.Join(_context.Profiles, profile => profile.ProfileId, user => user.ProfileId,
+                (profile, user) => new { profile, user }).Select(obj => obj.profile);
+            return await result.ToListAsync();
+        }
+    }
+}
